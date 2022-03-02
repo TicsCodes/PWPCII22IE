e07cafdd5302adce5acc17ec90db9a78548bc65a
TicsCodes <isaacitgam@gmail.com>
2022-03-02 09:12:39 -0600

    Modern js Babel app.js

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var aboutRouter = require('./routes/about');
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import aboutRouter from './routes/about';
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,12 +27,12 @@ app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,5 +41,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+//Exportando instancia de app usando js moderno
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let emojiDataset = ['🐸','🐲', '🐈‍⬛', '🐾', '🦬', '🐉','🦖' ];
+  let emoji = emojiDataset[Math.floor(Math.random()* emojiDataset.length)];
+
+  res.render('index', 
+  {
+  title: 'Express',
+  author: 'IsaacE',
+  emoji
+});
 });
 
 module.exports = router;
