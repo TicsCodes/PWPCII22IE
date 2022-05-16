fce8c8ced19924de9a39ebe01c05100553290d52
TicsCodes <isaacitgam@gmail.com>
2022-05-16 08:05:03 -0500

    S36 About Route

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -13,7 +13,17 @@ const index = (req, res) => {
   res.render('index', viewModel);
 };
 
+// URL: Get /about
+const about = (req, res) => {
+  res.render('home/aboutView', {
+    name: 'Isaac Estrada',
+    email: 'isaacitgam@gmail.com',
+    url: 'https://github.com/TicsCodes/PWPCII22IE',
+  });
+};
+
 export default {
   // action methods
   index,
+  about,
 };
